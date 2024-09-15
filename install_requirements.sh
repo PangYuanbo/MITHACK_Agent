eebcbcc158ff22b4f38f5c30090bd5413a12028b
Andy Phu <phu00003@umn.edu>
2024-09-15 01:50:03 -0400

    shell scripts for andy

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,13 +1,21 @@
+# Step 1: Create a virtual environment
 python3 -m venv path/to/venv
+
+# Step 2: Activate the virtual environment
 source path/to/venv/bin/activate
 
+virtualenv -p $(which python3) path/to/venv
+
+# Step 3: Update the PATH (This is for Anaconda, so if you're not using it in your virtual environment, you can skip this)
 export PATH="/opt/homebrew/anaconda3/bin:$PATH"
-python3 -m pip install google.protobuf.timestamp_pb2
 
-python3 -m pip install fastapi[standard]
-python3 -m pip install uvicorn[standard]
-python3 -m pip install httpx[standard]
-python3 -m pip install uagents
-python3 -m pip install protobuf
+# Step 4: Install the required Python packages
+pip install fastapi[standard]
+pip install uvicorn[standard]
+pip install httpx[standard]
+pip install uagents
+pip install google
+pip install google-cloud
+pip install protobuf
+pip install openai
 
-python3 -m pip install openai
---
+++ mac_fix.sh
@@ -0,0 +1,3 @@
+python3 -m venv path/to/venv
+source path/to/venv/bin/activate
+touch path/to/venv/lib/python3.12/site-packages/google/__init__.py
